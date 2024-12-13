c51e0c387dd7621ae0bea1cd3a27c229f3bb672f
TakatoEGLM <lightningmaverickmc@gmail.com>
2024-12-13 16:51:00 +0800

    Update OOPPythonLab5-main.py

--- OOPPythonLab5-main.py
+++ OOPPythonLab5-main.py
@@ -27,37 +27,37 @@ class LibraryApp:
         self.login()
         while True:
             print("\n=== Main Menu ===")
-            print("1. View the library catalog")
-            print("2. Search books from the library")
-
+            print("1. View the library catalog.")
+            print("2. Search for a book in the library.")
             if isinstance(self.current_user, (AdminUser, OperatorUser)):
-                print("3. Add a book to the library")
-                print("4. Edit a book from the library")
-                print("5. Remove a book from the library")
-                print("6. Mark all books from library as unavailable")
-            
-            if isinstance(self.current_user, (OperatorUser)):
-                print("7. Clear the library catalog")
-
-            print("\n8. Log out")
+                print("3. Add a book to the library.")
+                print("4. Edit a book from the library.")
+                print("5. Remove a book from the library.")
+                print("6. Mark all books as unavailable.")
+            if isinstance(self.current_user, OperatorUser):
+                print("7. Clear all books in the library catalog.")
+            print("8. Log out")
             print("0. Exit Library")
 
-            choice = input("\nChoose an option (1-8/0): ").strip()
+            choice = input("Choose an option (1-7/0): ").strip()
 
             if choice == "1":
                 self.current_user.view_library(self.library)
             elif choice == "2":
-                self.current_user.search_books(self.library)
+                keyword = input("Enter a keyword to search: ").strip()
+                self.library.search_books(keyword)
             elif choice == "3" and isinstance(self.current_user, (AdminUser, OperatorUser)):
                 self.current_user.add_book(self.library)
             elif choice == "4" and isinstance(self.current_user, (AdminUser, OperatorUser)):
-                self.current_user.edit_book(self.library)
+                isbn = input("Enter the ISBN of the book to edit: ").strip()
+                self.library.edit_book(isbn)
             elif choice == "5" and isinstance(self.current_user, (AdminUser, OperatorUser)):
-                self.current_user.remove_book(self.library)
+                isbn = input("Enter the ISBN of the book to remove: ").strip()
+                self.library.remove_book(isbn)
             elif choice == "6" and isinstance(self.current_user, (AdminUser, OperatorUser)):
-                self.current_user.mark_all_unavailable(self.library)
-            elif choice == "7" and isinstance(self.current_user, (OperatorUser)):
-                self.current_user.remove_all_books(self.library)
+                self.library.mark_all_unavailable()
+            elif choice == "7" and isinstance(self.current_user, OperatorUser):
+                self.library.remove_all_books(self.library)
             elif choice == "8":
                 self.logout()
             elif choice == "0":
@@ -69,6 +69,5 @@ class LibraryApp:
 
 if __name__ == "__main__":
     print("=== Welcome to Library ===\n")
-    book1 = Book("titlee","authorr","0000000000000","genree")
     app = LibraryApp()
     app.main_menu()
